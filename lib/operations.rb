cbe7a3c323587e6440662a23424690a254f1c11f
Otabek Kamalov <otakam86@gmail.com>
2018-07-20 18:40:47 +0000

    automatically backed up by learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,17 +1,15 @@
 def unsafe?(speed)
-  if speed < 30 || speed > 60
+  if speed < 40 || speed > 60
     true
 else
   false
 end
-
-
 end
 
 
 
 def not_safe?(speed)
-	
+speed < 40 || speed > 60 ? true : false	
 end
 	
 
